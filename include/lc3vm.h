d228eeb11e1d3b3c9234940e9c698a4557803eed
Donghyeon Baek <dbaek@leomail.tamuc.edu>
2026-04-20 04:34:02 +0000

    Complete Task 4

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -75,6 +75,7 @@ extern uint16_t PC_START;
 
 // your task functions should go here
 
+void update_flags(enum registr r);
 uint16_t sign_extend(uint16_t bits, int size);
 uint16_t mem_read(uint16_t address);
 void mem_write(uint16_t address, uint16_t value);
